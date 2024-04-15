57299b46795d2fec71be5c65c5c675878a11a823
Evgeny Boganov <boganov@gmail.com>
2024-04-15 13:43:58 +0300

    Added localization

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale locale = new Locale("en", "US");
+        Locale locale_ru_RU = new Locale("ru", "RU");
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("labels", locale_ru_RU);
+
+        System.out.println(resourceBundle.getString("hello"));
     }
 }
